5cde0746df8f9f78e87957867f69e71a92a230fb
zudevon <devon.rasch@degreeanalytics.com>
2018-06-22 16:07:25 -0500

    updated - file to rip csv into a new json

--- ap_upload_tm/apUploadTMDevon.py
+++ ap_upload_tm/apUploadTMDevon.py
@@ -12,60 +12,77 @@ sheet2 = pd.read_csv("APs/Sheet2-Table 1.csv")
 bssids.columns = ['room', 'name', 'mac', 'bssids', 'del']
 del bssids['del']
 
-# csvfile = open("APs/BSSIDs-Table 1.csv", 'r')
-# jsonfile = open('file.json', 'w')
-#
-# fieldnames = ("1","mac","bssids","4", "5")
-# reader = csv.DictReader(csvfile, fieldnames)
-# for row in reader:
-#     json.dump(row, jsonfile)
-#     jsonfile.write('\n')
-#
-#
-# json_data = open('file.json').read()
-#
-# json_file = json.loads(json_data)
-
-# print(json_file)
-
 print(bssids['name'][0])
 # print(bssids[0][0])
 
 print(len(bssids['name'])) # 479 rows in bssids df
 
-print(bssids['name'][1])
-
 import numpy as np
 
 """
-import json
 
 data = {}
 data['key'] = 'value'
 json_data = json.dumps(data)
+
 """
+counter = 0
+tm_aps = {}
+data = {}
 
-for i in range(0, 479):
 
-    if bssids['name'][i] != np.nan:  # check to see if you have an item in the row.
+for i in range(0, 478):
+
+    temp_dict = {}
+    temp_dict2 = {}
+
+
+    if pd.notna(bssids['name'][i]):  # check to see if you have an item in the row.
 
         # add name
-        print(bssids['name'][i])
-        # add room number from first column
-        print(bssids['room'][i])
-        # add mac address
-        print(bssids['mac'][i])
-        # add first bssids
-        print(bssids['bssids'][i])
+        name = bssids['name'][i]
+        # print(bssids['name'][i])
 
-        while bssids['bssids'][i] != np.nan:
+        # add room number from first column
+        temp_dict['room'] =  bssids['room'][i]
+        # print(bssids['room'][i])
+
+        # add mac address
+        temp_dict['mac'] = bssids['mac'][i]
+        # print(bssids['mac'][i])
+
+        # add first bssids
+        bssid_list = []
+        bssid_list.append(bssids['bssids'][i])
+
+        # print(bssids['bssids'][i])
+
+        while pd.notna(bssids['bssids'][i+1]):
             i += 1
 
             # add all bssid's for this name
-            if i < 479:
-                if bssids['bssids'][i]:
-                    print(bssids['bssids'][i])
-            if bssids['bssids'][i] == np.nan:
+            if i <= 478 and pd.notna(bssids['bssids'][i]):
+
+                bssid_list.append(bssids['bssids'][i])
+                # print(bssids['bssids'][i])
+
+            if i == 478:
                 break
 
+        temp_dict['bssids'] = bssid_list
 
+        counter += 1
+
+        tm_aps[name] = temp_dict
+
+# print(tm_aps)
+# print(temp_dict)
+
+tm_ap_json = json.dumps(tm_aps)
+# print(tm_ap_json)
+
+jsonfile = open('file.json', 'w')
+
+# json.dump(dictionary, file_to_write_to, indent=4)
+
+json.dump(tm_aps, jsonfile, indent=4)
